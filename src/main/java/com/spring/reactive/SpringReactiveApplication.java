80f63caf1e93f85baba904481ff28b2879f37751
Avadhut Jagtap <112001338+JagtapAvadhut@users.noreply.github.com>
2025-01-13 19:45:34 +0530

    Update SpringReactiveApplication.java
    added url

--- src/main/java/com/spring/reactive/SpringReactiveApplication.java
+++ src/main/java/com/spring/reactive/SpringReactiveApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringReactiveApplication {
-
+//mongodb://root:password@localhost:27017 -mongoDB compass
 	public static void main(String[] args) {
 		SpringApplication.run(SpringReactiveApplication.class, args);
 	}
